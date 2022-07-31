9902ebe350148a1e1c861487636ba312c5869f7e
Sediq <sediqabdullahi04@gmail.com>
2022-07-31 19:12:49 +0100

    hey

--- api/api.js
+++
@@ -1,8 +0,0 @@
-const express = require("express"),
-  app = express(),
-  dotenv = require("dotenv").config(),
-  starter = require("./db"),
-  Router = require("./modules/routes");
-
-app.use(Router);
-starter(app, "mongodb+srv://Trustadmin:Testimony1@cluster0.t9mbj.mongodb.net/");
